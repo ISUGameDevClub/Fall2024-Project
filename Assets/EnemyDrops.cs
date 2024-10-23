eec77dfc0e72cb39ab2a226c375d36d7ba389710
Muntenberg <55027521+Muntenberg@users.noreply.github.com>
2024-10-23 15:01:52 -0500

    Added comments to scripts for updates

--- Assets/EnemyDrops.cs
+++ Assets/EnemyDrops.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemyDrops : EnemyScript
 {
+    // MT: replace with the scriptable object of the items in here.
     public GameObject[] droppedItems;
     public float dropChances = .5f;
     // Start is called before the first frame update
@@ -16,8 +17,9 @@ public class EnemyDrops : EnemyScript
     {
         if(Random.value <= dropChances)
         {
+            // MT: Get the items name string and put it in the add Item Function for Inventory
             int randommap = Random.Range(0, droppedItems.Length);
-
+            // Import New Inventory system and use the add functions
             Instantiate(droppedItems[randommap], transform.position, Quaternion.identity);//Will be changed to inventory
         }
     }
