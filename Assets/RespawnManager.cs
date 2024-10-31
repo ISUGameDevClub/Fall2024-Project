87b094fdfa14811ef13627632d70d9423af05ea2
ScepCat25 <ethanlmenk@gmail.com>
2024-10-30 21:36:45 -0500

    Finished Respawn Manager
    Respawn manager was updated so it should work with scene transition and with the player health. Has "o" as a current test key to be removed when we know it works

--- Assets/RespawnManager.cs
+++ Assets/RespawnManager.cs
@@ -1,33 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RespawnManager : MonoBehaviour
 {
-    Health playerHeath;
-    DayNightCycle timeCycle;
+    public GameObject player;
+    Health playerHealth;
+    public DayNightCycle timeCycle;
+    public sceneTransition curtainsCloser;
     public Vector3 wakeLocation;
     public float timeSpentDead=9.0f;
     // Start is called before the first frame update
     void Start()
     {
-        playerHeath = FindObjectOfType<Health>();
-        //timeCycle = FindObjectOfType<DayNightCycle>();  //call a DNCycle 
-        
-        playerHeath.playerDeath += OnPlayerDeath;
+        playerHealth = player.GetComponent<Health>();
+        timeCycle = FindObjectOfType<DayNightCycle>();  //call a DNCycle 
+        wakeLocation = player.transform.position;
     }
 
     void OnPlayerDeath()
     {
         Debug.Log("Respawn Thingy Here");
-        //timeCycle.addTimeToPassing(timeSpentDead); //add time to DNCycle
+        timeCycle.addTimeToPassing(timeSpentDead); //add time to DNCycle
         //scene transition
+        
         //return player to wakeLocation 
+        player.transform.position = wakeLocation;
+        playerHealth.curHealth = playerHealth.maxHealth;
+        curtainsCloser.PlayerDeathTransition();
+        Debug.Log("Respawn Thingy there");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerHealth.curHealth < 1 || Input.GetKey("o"))
+        {
+            OnPlayerDeath();
+        }
+
     }
 }
