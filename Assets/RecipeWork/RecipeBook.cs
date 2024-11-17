fb64a301641bb3f00bbb2a156afd1b41cfd4c219
IWilcoski <isaiah.wilcoski@gmail.com>
2024-11-17 03:47:01 -0600

    FinishedRecipeBook
    Page flip fixed and added 2nd page recipes and code.

--- Assets/RecipeWork/RecipeBook.cs
+++ Assets/RecipeWork/RecipeBook.cs
@@ -26,6 +26,24 @@ public class RecipeBook : MonoBehaviour
         buttons[1].GetComponent<Button>().onClick.AddListener(Minus1);
         buttons[2].GetComponent<Button>().onClick.AddListener(Plus2);
         buttons[3].GetComponent<Button>().onClick.AddListener(Minus2);
+        buttons[4].GetComponent<Button>().onClick.AddListener(Plus3);
+        buttons[5].GetComponent<Button>().onClick.AddListener(Minus3);
+        buttons[6].GetComponent<Button>().onClick.AddListener(Plus4);
+        buttons[7].GetComponent<Button>().onClick.AddListener(Minus4);
+        buttons[8].GetComponent<Button>().onClick.AddListener(Plus5);
+        buttons[9].GetComponent<Button>().onClick.AddListener(Minus5);
+        buttons[10].GetComponent<Button>().onClick.AddListener(Plus6);
+        buttons[11].GetComponent<Button>().onClick.AddListener(Minus6);
+        buttons[12].GetComponent<Button>().onClick.AddListener(Plus7);
+        buttons[13].GetComponent<Button>().onClick.AddListener(Minus7);
+        buttons[14].GetComponent<Button>().onClick.AddListener(Plus8);
+        buttons[15].GetComponent<Button>().onClick.AddListener(Minus8);
+        buttons[16].GetComponent<Button>().onClick.AddListener(Plus9);
+        buttons[17].GetComponent<Button>().onClick.AddListener(Minus9);
+        buttons[18].GetComponent<Button>().onClick.AddListener(Plus10);
+        buttons[19].GetComponent<Button>().onClick.AddListener(Minus10);
+        buttons[20].GetComponent<Button>().onClick.AddListener(Plus11);
+        buttons[21].GetComponent<Button>().onClick.AddListener(Minus11);
         book[0].selectedNum = 0;
         book[1].selectedNum = 0;
         nextBack[0].GetComponent<Button>().onClick.AddListener(Next);
@@ -38,6 +56,7 @@ public class RecipeBook : MonoBehaviour
         if(pageNum == 1 && flip == true)
         {
             pages[0].transform.Rotate(0, 1, 0);
+            pages[2].SetActive(true);
             if (pages[0].GetComponent<RectTransform>().transform.eulerAngles.y > 90)
             {
                 pages[1].SetActive(true);
@@ -48,6 +67,7 @@ public class RecipeBook : MonoBehaviour
                 flip = false;
                 pageNum = 2;
                 GameObject.Find("RecipeBack").GetComponent<Button>().enabled = true;
+                GameObject.Find("RecipeNext").GetComponent<Button>().enabled = false;
             }
         }
         if (pageNum == 2 && flipBack == true)
@@ -63,6 +83,7 @@ public class RecipeBook : MonoBehaviour
                 flipBack = false;
                 pageNum = 1;
                 GameObject.Find("RecipeBack").GetComponent<Button>().enabled = false;
+                GameObject.Find("RecipeNext").GetComponent<Button>().enabled = true;
             }
         }
     }
@@ -122,6 +143,178 @@ public class RecipeBook : MonoBehaviour
             }
         }
     }
+    void Plus3()
+    {
+        tempInventory[5].quantity -= 1;
+        tempInventory[6].quantity -= 1;
+        book[2].selectedNum++;
+        GameObject.Find("Num3").GetComponent<TMP_Text>().text = book[2].selectedNum.ToString();
+        buttons[5].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+    void Plus4()
+    {
+        tempInventory[3].quantity -= 1;
+        tempInventory[4].quantity -= 1;
+        book[3].selectedNum++;
+        GameObject.Find("Num4").GetComponent<TMP_Text>().text = book[3].selectedNum.ToString();
+        buttons[7].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+    void Plus5()
+    {
+        tempInventory[3].quantity -= 1;
+        tempInventory[4].quantity -= 1;
+        tempInventory[8].quantity -= 1;
+        book[4].selectedNum++;
+        GameObject.Find("Num5").GetComponent<TMP_Text>().text = book[4].selectedNum.ToString();
+        buttons[9].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+    void Plus6()
+    {
+        tempInventory[6].quantity -= 1;
+        book[5].selectedNum++;
+        GameObject.Find("Num6").GetComponent<TMP_Text>().text = book[5].selectedNum.ToString();
+        buttons[11].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+    void Plus7()
+    {
+        tempInventory[0].quantity -= 1;
+        book[6].selectedNum++;
+        GameObject.Find("Num7").GetComponent<TMP_Text>().text = book[6].selectedNum.ToString();
+        buttons[13].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+    void Plus8()
+    {
+        tempInventory[1].quantity -= 1;
+        tempInventory[5].quantity -= 1;
+        book[7].selectedNum++;
+        GameObject.Find("Num8").GetComponent<TMP_Text>().text = book[7].selectedNum.ToString();
+        buttons[5].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+    void Plus9()
+    {
+        tempInventory[3].quantity -= 1;
+        tempInventory[4].quantity -= 1;
+        tempInventory[6].quantity -= 1;
+        book[8].selectedNum++;
+        GameObject.Find("Num9").GetComponent<TMP_Text>().text = book[8].selectedNum.ToString();
+        buttons[7].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+    void Plus10()
+    {
+        tempInventory[1].quantity -= 1;
+        tempInventory[4].quantity -= 1;
+        tempInventory[5].quantity -= 1;
+        book[9].selectedNum++;
+        GameObject.Find("Num10").GetComponent<TMP_Text>().text = book[9].selectedNum.ToString();
+        buttons[9].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
+    void Plus11()
+    {
+        tempInventory[2].quantity -= 1;
+        tempInventory[6].quantity -= 1;
+        book[10].selectedNum++;
+        GameObject.Find("Num11").GetComponent<TMP_Text>().text = book[10].selectedNum.ToString();
+        buttons[10].GetComponent<Button>().interactable = true;
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity < book[i].ingredientNum[j])
+                {
+                    buttons[i * 2].GetComponent<Button>().interactable = false;
+                }
+            }
+        }
+    }
     void Minus1()
     {
         tempInventory[0].quantity += 1;
@@ -165,7 +358,196 @@ public class RecipeBook : MonoBehaviour
             if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
         }
     }
+    void Minus3()
+    {
+        tempInventory[5].quantity += 1;
+        tempInventory[6].quantity += 1;
+        book[2].selectedNum--;
+        GameObject.Find("Num3").GetComponent<TMP_Text>().text = book[2].selectedNum.ToString();
+        if (book[2].selectedNum == 0) { buttons[5].GetComponent<Button>().interactable = false; }
 
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
+    void Minus4()
+    {
+        tempInventory[3].quantity += 1;
+        tempInventory[4].quantity += 1;
+        book[3].selectedNum--;
+        GameObject.Find("Num4").GetComponent<TMP_Text>().text = book[3].selectedNum.ToString();
+        if (book[3].selectedNum == 0) { buttons[7].GetComponent<Button>().interactable = false; }
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
+    void Minus5()
+    {
+        tempInventory[3].quantity += 1;
+        tempInventory[4].quantity += 1;
+        tempInventory[8].quantity += 1;
+        book[4].selectedNum--;
+        GameObject.Find("Num5").GetComponent<TMP_Text>().text = book[4].selectedNum.ToString();
+        if (book[4].selectedNum == 0) { buttons[9].GetComponent<Button>().interactable = false; }
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
+    void Minus6()
+    {
+        tempInventory[6].quantity += 1;
+        book[5].selectedNum--;
+        GameObject.Find("Num6").GetComponent<TMP_Text>().text = book[5].selectedNum.ToString();
+        if (book[5].selectedNum == 0) { buttons[11].GetComponent<Button>().interactable = false; }
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
+    void Minus7()
+    {
+        tempInventory[0].quantity += 1;
+        book[6].selectedNum--;
+        GameObject.Find("Num7").GetComponent<TMP_Text>().text = book[6].selectedNum.ToString();
+        if (book[6].selectedNum == 0) { buttons[13].GetComponent<Button>().interactable = false; }
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
+    void Minus8()
+    {
+        tempInventory[1].quantity += 1;
+        tempInventory[5].quantity += 1;
+        book[7].selectedNum--;
+        GameObject.Find("Num8").GetComponent<TMP_Text>().text = book[7].selectedNum.ToString();
+        if (book[7].selectedNum == 0) { buttons[15].GetComponent<Button>().interactable = false; }
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
+    void Minus9()
+    {
+        tempInventory[3].quantity += 1;
+        tempInventory[4].quantity += 1;
+        tempInventory[6].quantity += 1;
+        book[8].selectedNum--;
+        GameObject.Find("Num9").GetComponent<TMP_Text>().text = book[8].selectedNum.ToString();
+        if (book[8].selectedNum == 0) { buttons[17].GetComponent<Button>().interactable = false; }
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
+    void Minus10()
+    {
+        tempInventory[1].quantity += 1;
+        tempInventory[4].quantity += 1;
+        tempInventory[5].quantity += 1;
+        book[9].selectedNum--;
+        GameObject.Find("Num10").GetComponent<TMP_Text>().text = book[9].selectedNum.ToString();
+        if (book[9].selectedNum == 0) { buttons[19].GetComponent<Button>().interactable = false; }
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
+    void Minus11()
+    {
+        tempInventory[2].quantity += 1;
+        tempInventory[6].quantity += 1;
+        book[10].selectedNum--;
+        GameObject.Find("Num11").GetComponent<TMP_Text>().text = book[10].selectedNum.ToString();
+        if (book[10].selectedNum == 0) { buttons[21].GetComponent<Button>().interactable = false; }
+
+        for (int i = 0; i < book.Length; i++)
+        {
+            int tmpSpot = 0;
+            for (int j = 0; j < book[i].ingredientIndices.Length; j++)
+            {
+                if (tempInventory[book[i].ingredientIndices[j]].quantity >= book[i].ingredientNum[j])
+                {
+                    tmpSpot++;
+                }
+            }
+            if (tmpSpot == book[i].ingredientIndices.Length) { buttons[i * 2].GetComponent<Button>().interactable = true; }
+        }
+    }
     void Next()
     {
         flip = true;
